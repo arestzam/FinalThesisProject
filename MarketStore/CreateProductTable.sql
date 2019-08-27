3b300d480c1f5e0bba03e559d10ac24ab15b41a4
arestzam <45659288+arestzam@users.noreply.github.com>
2019-08-27 15:17:05 +0100

    Add files via upload

---
+++ MarketStore/CreateProductTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Product(
+         ProductID  INT,
+		 SupplierID INT,
+		 CategoryID INT,
+		 Name varchar(255),
+		 Price varchar(255),
+		 ProdDescr varchar(255),
+		 ProdUrl varchar(255)
+
+		 );
+
---
+++ MarketStore/dbo.Product.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+	[string] NAME
+    
+	)
